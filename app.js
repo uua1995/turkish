479faa14f5bf268392f4d531c34a6e7dfe993819
uua1995 <ulbosinuzakbaeva1995@gmail.com>
2023-10-25 22:21:02 +0300

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// showWord();
 async function asyncReadFile(filename) {
     try {
     const response = await fetch(filename);
@@ -8,8 +9,23 @@ async function asyncReadFile(filename) {
     console.log(err);
     }
 }
+
+
+var e, value;
+var eCh, valueCh;
+value = "a1"
+valueCh = "u1"
+
+function selectGrade() {
+    e = document.getElementById("grade");
+    value = e.value;
+    eCh = document.getElementById("unite");
+    valueCh = eCh.value;
+    showWord();
+}
+
 async function showWord() {
-    const words = await asyncReadFile('data.txt');
+    const words = await asyncReadFile('dictionaries/' + value + valueCh + '.txt');
     const num = document.getElementById("number_of_words");
     const len = words.length;
     const randomIndex = Math.floor(Math.random() * len);
@@ -17,8 +33,8 @@ async function showWord() {
     let myArray
     num.innerHTML = randomIndex + 1 + " / " + len;
     word = words[randomIndex];
-    myArray = word.split("-");
+    myArray = word.split(".");
     console.log(myArray[0] + " - " + myArray[1])
-    document.getElementById('word').innerHTML = myArray[0];
-    document.getElementById('def').innerHTML = myArray[1];
+    document.getElementById('word').innerHTML = myArray[0][0].toUpperCase() + myArray[0].slice(1);
+    document.getElementById('def').innerHTML = myArray[1][0].toUpperCase() + myArray[1].slice(1);
 }
