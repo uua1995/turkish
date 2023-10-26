d721e0a95ca7fd00b65421b8a0a07d1e12fdd483
uua1995 <ulbosinuzakbaeva1995@gmail.com>
2023-10-26 11:32:26 +0300

    responsive header

--- app.js
+++ app.js
@@ -45,3 +45,12 @@ async function showWord() {
     document.getElementById('word').innerHTML = myArray[0][0].toUpperCase() + myArray[0].slice(1);
     document.getElementById('def').innerHTML = myArray[1][0].toUpperCase() + myArray[1].slice(1);
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
