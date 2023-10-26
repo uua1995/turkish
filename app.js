84ba34e2fd01586322f5577f31c37c96e2ebef8e
uua1995 <ulbosinuzakbaeva1995@gmail.com>
2023-10-27 02:01:26 +0500

    11111

--- app.js
+++ app.js
@@ -54,3 +54,28 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  // accordion
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
